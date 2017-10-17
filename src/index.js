34ec9c8e016454c4e9bfd964b5e06e4ee3d35fd6
Robin Watson <drumsrobin@gmail.com>
2017-10-17 09:09:22 -0400

    fixed spelling

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-let registeredEarly = true;
+let registeredEarly = false;
 
-let runnersAge = 18;
+let runnersAge = 19;
 
 let raceNumber =  Math.floor( Math.random() * 1000 );
 
-!registeredEarly ? console.log('Late') : console.log( raceNumber += 1000);
+registeredEarly ? console.log('On time!') : console.log( raceNumber += 1000);
 
 if (runnersAge > 18 && registeredEarly) {
   console.log(`You run at 9:30am. Race number: ${raceNumber}`);
-} else if (registeredEarly || runnnersage > 18) {
+} else if (registeredEarly || runnersAge > 18) {
   console.log(`You run at 11am. Race number: ${raceNumber}`);
-} else if (runnersAge < 18 && !registeredEarly) {
+} else if (runnersAge < 18 && registeredEarly) {
   console.log(`You start at 12:30am. Race number ${raceNumber}`)
 } else {
   console.log('see front desk');
